0f9500c60453d1b716a078e327e56e847338fd59
Ephron Agyeman-Opoku <94077473+Ephfullstack@users.noreply.github.com>
2022-11-29 11:17:34 +0000

    added context

--- spec/spell_spec.rb
+++ spec/spell_spec.rb
@@ -5,19 +5,26 @@ describe Spell do
     before(:each) do 
         @Spell = Spell.new
     end
+    context 'correctly spelled words' do 
 
-    it 'returns currently spelled word as it is' do
+        it 'returns currently spelled word as it is' do
 
-        expect(@Spell.check("Hello")).to eq "Hello"
-
-    end
-
+            expect(@Spell.check("Hello")).to eq "Hello"
     
-    it 'contains word bank when initialised' do
-        expect(@Spell.word_bank).to be_a Array
-    end
-
-    it 'return word surronded by ~ if it does not match a word in word bank array' do
-        expect(@Spell.check("a")).to eq '~a~'
+        end
     end 
+    
+
+    context 'incorrectly spelled words' do 
+        it 'contains word bank when initialised' do
+            expect(@Spell.word_bank).to be_a Array
+        end
+    
+        it 'return word surronded by ~ if it does not match a word in word bank array' do
+            expect(@Spell.check("a")).to eq '~a~'
+        end 
+    end 
+
+   
+
 end
