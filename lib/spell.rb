e40082deb8539b02407ecbcddaa17571e6e624ca
Ephron Agyeman-Opoku <94077473+Ephfullstack@users.noreply.github.com>
2022-11-29 11:13:13 +0000

    added a method which returns in correct words

--- lib/spell.rb
+++ lib/spell.rb
@@ -3,10 +3,14 @@ class Spell
     attr_reader :word_bank
 
     def initialize
-        @word_bank = ["as"];
+        @word_bank = ["Hello"];
     end
-
+    
     def check(string)
+        if @word_bank.include? (string)
         return string
+        else 
+        return "~#{string}~"
+        end 
     end
 end
--- spec/spell_spec.rb
+++ spec/spell_spec.rb
@@ -11,8 +11,13 @@ describe Spell do
         expect(@Spell.check("Hello")).to eq "Hello"
 
     end
+
     
     it 'contains word bank when initialised' do
         expect(@Spell.word_bank).to be_a Array
     end
+
+    it 'return word surronded by ~ if it does not match a word in word bank array' do
+        expect(@Spell.check("a")).to eq '~a~'
+    end 
 end
